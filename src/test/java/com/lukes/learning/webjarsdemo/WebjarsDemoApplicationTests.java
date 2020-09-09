48ff0cb22cf3c94db64d2df0932ec60e1d5ccc8e
lukes8 <gw.lusi@seznam.cz>
2020-09-09 15:07:18 +0200

    Added webJars demo example to introduce bootstrap, jquery on web page

---
+++ src/test/java/com/lukes/learning/webjarsdemo/WebjarsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lukes.learning.webjarsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebjarsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
